294e630c47d5f5835830adff0612b9c840feba87
Qingjie <qingjie1908@outlook.com>
2024-07-16 19:17:54 +0800

    C5

---
+++ python_crash_course/C5/5p2.py
@@ -0,0 +1,10 @@
+l1 = list(range(1,10,2))
+print(l1) #[1, 3, 5, 7, 9]
+
+a, b = 1, 2
+print(a == 1 and b == 2) #True
+print(a == 1 or b == 3) #True
+print(1 in l1) #True
+print(10 in l1) #False
+print(2 not in l1) #True
+print(3 not in l1) #False
